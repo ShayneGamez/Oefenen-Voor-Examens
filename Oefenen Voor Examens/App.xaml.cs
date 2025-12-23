b41a637504195d1ca53ad8786a89c449d4a7a544
ShayneGamez07 <Shayne.Vanoutryve@student.howest.be>
2025-12-23 16:03:51 +0100

    Add project files.

---
+++ Oefenen Voor Examens/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Oefenen_Voor_Examens
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
